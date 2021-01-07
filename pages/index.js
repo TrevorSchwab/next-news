caa2579092fa306f911537106ba8ee831b2fa5b4
Trevor Schwab <trevschwab@gmail.com>
2021-01-07 17:26:11 -0600

    added skeleton for index.js and created toolbar

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,11 @@ import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
-    <div></div>
+    <div className='page-container'>Home
+      <div className={styles.main}>
+        <h1>Next.js News App</h1>
+        <h3>Your one stop shop for the latest news articles</h3>
+      </div>
+    </div>
   )
 }
