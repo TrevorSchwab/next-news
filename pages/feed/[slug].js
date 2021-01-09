bc60bd9df5f8b25715d9297be78c3eb7d0cf97a2
Trevor Schwab <trevschwab@gmail.com>
2021-01-09 14:50:52 -0600

    feed page is now styled and functional

--- pages/feed/[slug].js
+++ pages/feed/[slug].js
@@ -1,8 +1,39 @@
-export const Feed = ({ pageNumber, articles }) => {
+import styles from '../../styles/Feed.module.css';
+import { useRouter } from 'next/router';
+import { Toolbar } from '../../components/toolbar'
 
-  console.log(articles, pageNumber)
+export const Feed = ({ pageNumber, articles }) => {
+  const router = useRouter();
   return (
-    <>HelloWorld</>
+    <div className='page-container'>
+      <Toolbar />
+      <div className={styles.main}>
+        {articles.map((article, index) => (
+          <div key={index} className={styles.post}>
+            <h1 onClick={() => (window.location.href = article.url)}>{article.title}</h1>
+            <p>{article.description}</p>
+            {!!article.urlToImage && <img src={article.urlToImage} />}
+          </div>
+        ))}
+      </div>
+      <div className={styles.paginator}>
+        <div onClick={() => {
+          if (pageNumber > 1) {
+            router.push(`/feed/${pageNumber - 1}`).then(() => window.scrollTo(0, 0))
+          }
+        }} className={pageNumber === 1 ? styles.disabled : styles.active}>
+          Previous Page
+        </div>
+        <div>#{pageNumber}</div>
+        <div onClick={() => {
+          if (pageNumber < 5) {
+            router.push(`/feed/${pageNumber + 1}`).then(() => window.scrollTo(0, 0))
+          }
+        }} className={pageNumber === 5 ? styles.disabled : styles.active}>
+          Next Page
+        </div>
+      </div>
+    </div>
   )
 };
 
