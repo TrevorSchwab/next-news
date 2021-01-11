00aadab4c64df5d2ab8be685d650846eb55deac2
Trevor Schwab <trevschwab@gmail.com>
2021-01-10 18:16:40 -0600

    hopefully fixing image url

--- pages/eom.js
+++ pages/eom.js
@@ -15,20 +15,22 @@ export const getServerSideProps = async pageContext => {
 }
 
 export const EOM = ({ employee }) => {
+  console.log(employee)
   return(
-    <div className='page-container'>
-      <Toolbar />
-      <div className={styles.main}>
-        <h1>Employee Of The Month</h1>
+    <div className="page-container">
+    <Toolbar />
 
-        <div className={styles.employeeOfTheMonth}>
-          <h3>employee name</h3>
-          <h6>employee position</h6>
-          <img />
-          <p>employee description</p>
-        </div>
-      </div>      
+    <div className={styles.main}>
+      <h1>Employee Of The Month</h1>
+
+      <div className={styles.employeeOfTheMonth}>
+        <h3>{employee.name}</h3>
+        <h6>{employee.position}</h6>
+        <img src={employee.image} alt="employee" />
+        <p>{employee.description}</p>
+      </div>
     </div>
+  </div>
   )
 }
 
